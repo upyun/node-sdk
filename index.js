72506c96035195d1505df2a1adb35fbfae56687b
yejingx <yejingx@gmail.com>
2016-05-25 11:20:51 +0800

    comp with old sdk

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 'use strict';
+var upyunDeprecated = require('upyun-legacy');
 var upyun = require('./upyun/api.js');
 
-module.exports = exports.UpYun = function(bucket, operator, password, endpoint) {
-    return new upyun(bucket, operator, password, endpoint);
+module.exports = exports.UPYUN = exports.UpYun = function(bucket, operator, password, endpoint, apiVersion) {
+    var client = null;
+    if(apiVersion === 2) {
+        return new upyun(bucket, operator, password, endpoint);
+    } else {
+        client = new upyunDeprecated(bucket, operator, password, endpoint);
+    }
+
+    client._apiVersion = apiVersion;
+
+    return client;
 }

798e28e797d4bf6785f323cd18068468fff3d04f
yejingx <yejingx@gmail.com>
2016-05-27 16:04:59 +0800

    rename

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 var upyunDeprecated = require('upyun-legacy');
-var rest = require('./upyun/rest.js');
+var upyun = require('./upyun/api.js');
 
 module.exports = exports.UPYUN = exports.UpYun = function(bucket, operator, password, endpoint, opts) {
   var client = null;
 
   if (opts && opts.apiVersion == 'v2') {
-    client = new rest(bucket, operator, password, endpoint, opts);
+    client = new upyun(bucket, operator, password, endpoint, opts);
   } else {
     client = new upyunDeprecated(bucket, operator, password, endpoint);
   }

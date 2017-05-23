49d4fcd5a43fbd1d6bdb89896f9f6592982b5a69
Gaara <sabakugaara@users.noreply.github.com>
2017-05-23 18:45:05 +0800

    feat add blockUpload

---
+++ tests/browser-index.js
@@ -0,0 +1,25 @@
+'use strict'
+
+import { expect } from 'chai'
+import Upyun from '../upyun/index'
+
+const client = new Upyun({
+  bucket: 'sdkimg',
+  operator: 'tester',
+  password: 'grjxv2mxELR3'
+})
+
+describe('index', () => {
+  describe('#blockUpload', () => {
+    it('should upload file success', async () => {
+      const f = new Blob(['text'], {type: 'text/plain'})
+      f.name = 'testBlockUpload.txt'
+
+      const result = await client.blockUpload('/testBlockUpload.txt', f, {
+        'Content-Length': 4,
+        'Content-Type': 'text/plain'
+      })
+      expect(result).to.equal(true)
+    })
+  })
+})

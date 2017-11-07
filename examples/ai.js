701eacc60101ce8b9823cb38310f92a77588a33d
xiang.fei <xiang.fei@upai.com>
2017-11-07 17:54:27 +0800

    update

--- examples/ai.js
+++ examples/ai.js
@@ -5,19 +5,19 @@
  */
 'use strict'
 
-import Upyun from "../upyun/upyun"
-import Service from "../upyun/service"
+import Upyun from '../upyun/upyun'
+import Service from '../upyun/service'
 import fs from 'fs'
 
 // 需要填写自己的服务名，操作员名，密码，通知URL
-const serviceName = ""
-const operatorName = ""
-const password = ""
-const notifyUrl = ""
+const serviceName = ''
+const operatorName = ''
+const password = ''
+const notifyUrl = ''
 
 // 需要填写本地路劲，云存储路径
-const localFile = ""
-const remoteFile = ""
+const localFile = ''
+const remoteFile = ''
 
 const client = new Upyun(new Service(serviceName, operatorName, password))
 
@@ -25,20 +25,21 @@ const client = new Upyun(new Service(serviceName, operatorName, password))
  * 内容识别-图片上传预处理
  */
 function imageFormProcess() {
-    var res
-    // params 参数详见人工智能，云存储参数说明文档
-    var params = { "apps": [{ "name": "imgaudit" }], "notify-url": notifyUrl }
-    res = client.formPutFile(remoteFile, fs.createReadStream(localFile), params)
-    console.log(res)
+  var res
+  // params 参数详见人工智能，云存储参数说明文档
+  var params = { 'apps': [{ 'name': 'imgaudit' }], 'notify-url': notifyUrl }
+  client.formPutFile(remoteFile, fs.createReadStream(localFile), params)
 }
 
 /**
  * 内容识别-视频点播上传预处理
  */
 function videoFormProcess() {
-    var res
-    // params 参数详见人工智能，云存储参数说明文档
-    var params = { "apps": [{ "name": "videoaudit" }], "notify-url": notifyUrl }
-    res = client.formPutFile(remoteFile, fs.createReadStream(localFile), params)
-    console.log(res)
+  var res
+  // params 参数详见人工智能，云存储参数说明文档
+  var params = { 'apps': [{ 'name': 'videoaudit' }], 'notify-url': notifyUrl }
+  client.formPutFile(remoteFile, fs.createReadStream(localFile), params)
 }
+
+imageFormProcess()
+videoFormProcess()
--- examples/process.js
+++ examples/process.js
@@ -5,20 +5,20 @@
  */
 'use strict'
 
-import Upyun from "../upyun/upyun"
-import Service from "../upyun/service"
+import Upyun from '../upyun/upyun'
+import Service from '../upyun/service'
 import fs from 'fs'
 
 // 需要填写自己的服务名，操作员名，密码，通知URL
-const serviceName = ""
-const operatorName = ""
-const password = ""
-const notifyUrl = ""
+const serviceName = ''
+const operatorName = ''
+const password = ''
+const notifyUrl = ''
 
 // 需要填写本地路劲，云存储路径
-const localFile = ""
-const remoteFile = ""
-const saveAs = ""
+const localFile = ''
+const remoteFile = ''
+const saveAs = ''
 
 const client = new Upyun(new Service(serviceName, operatorName, password))
 
@@ -26,53 +26,54 @@ const client = new Upyun(new Service(serviceName, operatorName, password))
  * form同步图片上传预处理
  */
 function imageFormSyncProcess() {
-    var res
-    // params 参数详见云处理，云存储参数说明文档
-    var params = { "x-gmkerl-thumb": "/format/png", "notify-url": notifyUrl }
-    res = client.formPutFile(remoteFile, fs.createReadStream(localFile), params)
-    console.log(res)
+  var res
+  // params 参数详见云处理，云存储参数说明文档
+  var params = { 'x-gmkerl-thumb': '/format/png', 'notify-url': notifyUrl }
+  client.formPutFile(remoteFile, fs.createReadStream(localFile), params)
 }
 
 /**
  * rest同步图片上传预处理
  */
 function imageRestSyncProcess() {
-    var res
-    // params 参数详见云处理，云存储参数说明文档
-    var params = { "x-gmkerl-thumb": "/format/png", "notify-url": notifyUrl }
-    res = client.putFile(remoteFile,fs.createReadStream(localFile), params)
-    console.log(res)
+  var res
+  // params 参数详见云处理，云存储参数说明文档
+  var params = { 'x-gmkerl-thumb': '/format/png', 'notify-url': notifyUrl }
+  client.putFile(remoteFile, fs.createReadStream(localFile), params)
 }
 
 /**
  * 异步图片上传预处理
  */
 function imageASyncProcess() {
-    var res
-    // params 参数详见云处理，云存储参数说明文档
-    var params = { "apps": [{ "name": "thumb", "x-gmkerl-thumb": "/format/png", "save_as": saveAs }], "notify-url": notifyUrl }
-    res = client.formPutFile(remoteFile, fs.createReadStream(localFile), params)
-    console.log(res)
+  var res
+  // params 参数详见云处理，云存储参数说明文档
+  var params = { 'apps': [{ 'name': 'thumb', 'x-gmkerl-thumb': '/format/png', 'save_as': saveAs }], 'notify-url': notifyUrl }
+  client.formPutFile(remoteFile, fs.createReadStream(localFile), params)
 }
 
 /**
  * 异步音视频处理-上传预处理
  */
-function videoFormProcess(){
-    var res
-    // params 参数详见云处理，云存储参数说明文档
-    var params = { "apps": [{ "name": "naga", "type": "video","avopts": "/s/128x96", "save_as": saveAs }], "notify-url": notifyUrl }
-    res = client.formPutFile(remoteFile, fs.createReadStream(localFile), params)
-    console.log(res)
+function videoFormProcess() {
+  var res
+  // params 参数详见云处理，云存储参数说明文档
+  var params = { 'apps': [{ 'name': 'naga', 'type': 'video', 'avopts': '/s/128x96', 'save_as': saveAs }], 'notify-url': notifyUrl }
+  client.formPutFile(remoteFile, fs.createReadStream(localFile), params)
 }
 
 /**
  * 文档转换-上传预处理
  */
-function fileFormConvert(){
-    var res
-    // params 参数详见云处理，云存储参数说明文档
-    var params = { "apps": [{ "name": "uconvert", "save_as": saveAs }], "notify-url": notifyUrl }
-    res = client.formPutFile(remoteFile, fs.createReadStream(localFile), params)
-    console.log(res)
+function fileFormConvert() {
+  var res
+  // params 参数详见云处理，云存储参数说明文档
+  var params = { 'apps': [{ 'name': 'uconvert', 'save_as': saveAs }], 'notify-url': notifyUrl }
+  client.formPutFile(remoteFile, fs.createReadStream(localFile), params)
 }
+
+imageFormSyncProcess()
+imageRestSyncProcess()
+imageASyncProcess()
+videoFormProcess()
+fileFormConvert()

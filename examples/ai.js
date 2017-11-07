04ded869e021e8d5b3fd6e7bb25fa135867b772f
xiang.fei <xiang.fei@upai.com>
2017-11-07 18:29:26 +0800

    update ai

--- examples/ai.js
+++ examples/ai.js
@@ -10,33 +10,35 @@ import Service from '../upyun/service'
 import fs from 'fs'
 
 // 需要填写自己的服务名，操作员名，密码，通知URL
-const serviceName = ''
-const operatorName = ''
-const password = ''
-const notifyUrl = ''
+const serviceName = 'upyun-xiang-1'
+const operatorName = 'xiang1'
+const password = 'feixiang1218'
+const notifyUrl = 'http://101.132.47.61:12345/'
 
 // 需要填写本地路劲，云存储路径
-const localFile = ''
-const remoteFile = ''
+const localFile = './test.jpg'
+const remoteFile = '/test.jpg'
 
 const client = new Upyun(new Service(serviceName, operatorName, password))
+
+function auditProcess(name) {
+  // params 参数详见人工智能，云存储参数说明文档
+  var params = { 'apps': [{ 'name': name }], 'notify-url': notifyUrl }
+  client.formPutFile(remoteFile, fs.createReadStream(localFile), params)
+}
 
 /**
  * 内容识别-图片上传预处理
  */
 function imageFormProcess() {
-  // params 参数详见人工智能，云存储参数说明文档
-  var params = { 'apps': [{ 'name': 'imgaudit' }], 'notify-url': notifyUrl }
-  client.formPutFile(remoteFile, fs.createReadStream(localFile), params)
+  auditProcess('imgaudit')
 }
 
 /**
  * 内容识别-视频点播上传预处理
  */
 function videoFormProcess() {
-  // params 参数详见人工智能，云存储参数说明文档
-  var params = { 'apps': [{ 'name': 'videoaudit' }], 'notify-url': notifyUrl }
-  client.formPutFile(remoteFile, fs.createReadStream(localFile), params)
+  auditProcess('videoaudit')
 }
 
 imageFormProcess()

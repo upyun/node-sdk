ce417911d7ca5c5dc11548b61ea2984d47bd4f08
taevas <di4urp@gmail.com>
2019-07-15 11:54:29 +0800

    :sparkles: 浏览器端也支持 basename

--- upyun/browser-form-upload.js
+++ upyun/browser-form-upload.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import path from 'path'
 
 export default function formUpload (remoteUrl, localFile, {authorization, policy}, {filename} = {}) {
   const data = new FormData()
@@ -8,6 +9,7 @@ export default function formUpload (remoteUrl, localFile, {authorization, policy
     localFile = new Blob([localFile], {type: 'text/plain'})
   }
 
+  filename = filename ? path.basename(filename) : filename
   data.append('file', localFile, filename)
   return axios.post(remoteUrl, data).then(({status, data}) => {
     if (status === 200) {

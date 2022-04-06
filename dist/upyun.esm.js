1a9fecddab6a93fa4d9e389a633769d842497945
arrebole <concurrent.exec@gmail.com>
2022-04-06 17:17:14 +0800

    chore(deps): update base-64 from 0.1.0 to 1.0.0

--- dist/upyun.esm.js
+++ dist/upyun.esm.js
@@ -1815,7 +1815,7 @@ function b64_hmac_sha1(k,d,_p,_z){
 var hmacsha1 = b64_hmac_sha1;
 
 var base64 = createCommonjsModule(function (module, exports) {
-/*! http://mths.be/base64 v0.1.0 by @mathias | MIT license */
+/*! https://mths.be/base64 v1.0.0 by @mathias | MIT license */
 (function(root) {
 
 	// Detect free variables `exports`.
@@ -1908,7 +1908,6 @@ var base64 = createCommonjsModule(function (module, exports) {
 		var a;
 		var b;
 		var c;
-		var d;
 		var buffer;
 		// Make sure any padding is handled outside of the loop.
 		var length = input.length - padding;
@@ -1954,7 +1953,7 @@ var base64 = createCommonjsModule(function (module, exports) {
 	var base64 = {
 		'encode': encode,
 		'decode': decode,
-		'version': '0.1.0'
+		'version': '1.0.0'
 	};
 
 	// Some AMD build optimizers, like r.js, check for specific condition patterns
@@ -1997,7 +1996,7 @@ var bugs = { "url": "https://github.com/upyun/node-sdk/issues" };
 var homepage = "https://github.com/upyun/node-sdk";
 var contributors = [{ "name": "yejingx", "email": "yejingx@gmail.com" }, { "name": "Leigh", "email": "i@zhuli.me" }, { "name": "kaidiren", "email": "kaidiren@gmail.com" }, { "name": "Gaara", "email": "sabakugaara@users.noreply.github.com" }];
 var devDependencies = { "babel-cli": "^6.24.1", "babel-loader": "^7.0.0", "babel-plugin-external-helpers": "^6.22.0", "babel-plugin-transform-runtime": "^6.23.0", "babel-preset-env": "^1.4.0", "babel-register": "^6.24.1", "builtin-modules": "^3.1.0", "chai": "^3.5.0", "delay": "^4.2.0", "eslint": "^5.16.0", "istanbul": "^0.4.3", "karma": "^1.7.0", "karma-chrome-launcher": "^2.1.1", "karma-mocha": "^1.3.0", "karma-sourcemap-loader": "^0.3.7", "karma-webpack": "^2.0.3", "mocha": "^3.4.1", "rollup": "^0.41.6", "rollup-plugin-alias": "^1.3.1", "rollup-plugin-babel": "^2.7.1", "rollup-plugin-commonjs": "^8.0.2", "rollup-plugin-json": "^2.1.1", "rollup-plugin-node-builtins": "^2.1.2", "rollup-plugin-node-resolve": "^3.0.0", "should": "^9.0.2", "uglify-js": "^3.0.11", "webpack": "^2.5.1" };
-var dependencies = { "axios": "^0.26.1", "base-64": "^0.1.0", "form-data": "^3.0.0", "hmacsha1": "^1.0.0", "is-promise": "^2.1.0", "md5": "^2.2.1", "mime-types": "^2.1.15" };
+var dependencies = { "axios": "^0.26.1", "base-64": "^1.0.0", "form-data": "^3.0.0", "hmacsha1": "^1.0.0", "is-promise": "^2.1.0", "md5": "^2.2.1", "mime-types": "^2.1.15" };
 var browser = { "./upyun/utils.js": "./upyun/browser-utils.js", "./upyun/form-upload.js": "./upyun/browser-form-upload.js" };
 var pkg = {
 	name: name,
